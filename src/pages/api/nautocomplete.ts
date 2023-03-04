ed381e0301bbf2926657fbec2fa97e3355c92ae3
Arda Mutlu <ardamutlu@Ardas-MacBook-Pro.local>
2023-03-05 00:21:54 +0300

    add autocomplete api

---
+++ src/pages/api/nautocomplete.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { query } = req.query;
+  try {
+    const { data } = await axios.request({
+      url: `http://unsplash.com/nautocomplete/${query}`,
+      method: "GET",
+    });
+    res.status(200).json(data);
+  } catch (error: any) {
+    res.status(error.response.status).json(error);
+  }
+}
